db3f0763b3902ca93284e2ef50d8ae87cf9d0007
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-31 15:48:19 +0530

    tool-calling

--- app.js
+++ app.js
@@ -17,11 +17,36 @@ async function main(){
                 // When was iphone 16 launched?
             },
         ],
+        tools:[
+    {
+      "type": "function",
+      "function": {
+        "name": "get_current_weather",
+        "description": "Get the current weather in a given location",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "location": {
+              "type": "string",
+              "description": "The city and state, e.g. San Francisco, CA"
+            },
+            "unit": {
+              "type": "string",
+              "enum": ["celsius", "fahrenheit"]
+            }
+          },
+          "required": ["location"]
+        }
+      }
+    }
+  ],
     });
     console.log(completions.choices[0].message);
 }
 await main();
 
 async function webSearch({query}){
-    // here we will do 
+    // here we will do tavily api call
+
+    return "Iphone was launched on 20 september 2024.";
 }
