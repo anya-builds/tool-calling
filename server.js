b556528ea31e90b1b76296ee8f4d351b4a30e45b
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-03 23:08:24 +0530

    chat gpt clone

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import { generate } from './chatbot.js';
+// import express from 'express';
+// import cors from 'cors';
+// import { generate } from './chatbot.js';
 
-const app = express();
-const port = 3001;
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// const port = 3001;
+// app.use(cors());
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to ChatDPT!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Welcome to ChatDPT!');
+// });
 
-app.post('/chat', async (req, res) => {
-    const { message, threadId } = req.body;
-    // todo: validate above fields
+// app.post('/chat', async (req, res) => {
+//     const { message, threadId } = req.body;
+//     // todo: validate above fields
 
-    if (!message || !threadId) {
-        res.status(400).json({ message: 'All fields are required!' });
-        return;
-    }
+//     if (!message || !threadId) {
+//         res.status(400).json({ message: 'All fields are required!' });
+//         return;
+//     }
 
-    console.log('Message', message);
+//     console.log('Message', message);
 
-    const result = await generate(message, threadId);
-    res.json({ message: result });
-});
+//     const result = await generate(message, threadId);
+//     res.json({ message: result });
+// });
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on port: ${port}`);
+// });
